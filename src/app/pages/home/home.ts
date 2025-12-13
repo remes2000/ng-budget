a47aac07945d8e39a31f70fe37fb6f854c9892f2
Michał Nieruchalski <nieruchalskimichal@gmail.com>
2025-12-13 21:11:04 +0100

    add basic drawer

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatDrawer, MatDrawerContainer, MatDrawerContent } from '@angular/material/sidenav';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatDrawer, MatDrawerContainer, MatDrawerContent, MatIconButton, MatIcon],
   templateUrl: './home.html',
   styleUrl: './home.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Home {
+  drawerOpened = signal(true);
 
+  closeDrawer() {
+    this.drawerOpened.set(false);
+  }
+
+  openDrawer() {
+    this.drawerOpened.set(true);
+  }
 }
