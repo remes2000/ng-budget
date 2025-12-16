b7e278e6525877c650859fa0582357d130e27a5c
Michał Nieruchalski <nieruchalskimichal@gmail.com>
2025-12-16 22:39:45 +0100

    setup basic persistence layer

--- src/app/data-access/budget.service.ts
+++ src/app/data-access/budget.service.ts
@@ -1,34 +1,23 @@
 import { computed, inject, Injectable, signal } from '@angular/core';
 import { BudgetEntry, Category } from '@models';
 import { CATEGORIES } from '../mock-data/categories';
+import { ReactiveStorageService } from './reactive-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BudgetService {
+  #reactiveStorage = inject(ReactiveStorageService);
   #month = signal(new Date().getMonth() + 1);
   #year = signal(new Date().getFullYear());
   #storageKey = computed(() => `budget-${this.#month()}-${this.#year()}`);
 
   #entries = computed<BudgetEntry[]>(() => {
-    const data = localStorage.getItem(this.#storageKey());
+    const storageSignal = this.#reactiveStorage.getItem(this.#storageKey());
+    const data = storageSignal();
     return data ? JSON.parse(data) : [];
   });
 
-  // #entries = signal<BudgetEntry[]>([
-  //   {
-  //     id: crypto.randomUUID(),
-  //     amount: 12550,
-  //     category: 'zakup-spozywcze',
-  //     date: new Date('2025-12-10')
-  //   },
-  //   {
-  //     id: crypto.randomUUID(),
-  //     amount: 4500,
-  //     category: 'jedzenie-na-miescie',
-  //     date: new Date('2025-12-12')
-  //   }
-  // ]);
   entries = computed(() => this.#entries().map(entry => ({
     ...entry,
     amount: entry.amount / 100
@@ -46,7 +35,6 @@ export class BudgetService {
     this.#year.set(year);
   }
 
-  // TODO: this methods can't access #entries signal directly
   add(amount: number, category: string): BudgetEntry {
     const entry: BudgetEntry = {
       id: crypto.randomUUID(),
@@ -55,7 +43,7 @@ export class BudgetService {
       date: new Date()
     };
 
-    localStorage.setItem(
+    this.#reactiveStorage.setItem(
       this.#storageKey(),
       JSON.stringify([...this.#entries(), entry])
     );
@@ -64,10 +52,15 @@ export class BudgetService {
   }
 
   update(id: string, category: Category['id'], amount: number): void {
-    // this.#entries.update(entries => entries.map(entry =>
-    //   entry.id === id
-    //     ? { ...entry, category, amount: amount * 100 }
-    //     : entry
-    // ));
+    this.#reactiveStorage.setItem(
+      this.#storageKey(),
+      JSON.stringify(
+        this.#entries().map(entry =>
+          entry.id === id
+            ? { ...entry, category, amount: amount * 100 }
+            : entry
+        )
+      )
+    );
   }
 }
