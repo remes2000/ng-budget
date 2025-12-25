f9452eee1679e6870799d4f97f001e4625d3b394
Michał Nieruchalski <nieruchalskimichal@gmail.com>
2025-12-25 17:11:50 +0100

    add environment

--- src/app/const.ts
+++ src/app/const.ts
@@ -2,5 +2,7 @@ import { InjectionToken } from "@angular/core";
 import { TypedPocketBase } from "@models";
 
 export const PB = new InjectionToken<TypedPocketBase>('PocketBase client');
-export const API_URL = 'http://127.0.0.1:8090';
+import { environment } from '../environments/environment';
+
+export const API_URL = environment.apiUrl;
 
