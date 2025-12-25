aff81898a9fbb7eed14e9ad46315e8f11d40a581
Michał Nieruchalski <nieruchalskimichal@gmail.com>
2025-12-25 15:22:55 +0100

    handle error in report

--- src/app/pages/report/report.ts
+++ src/app/pages/report/report.ts
@@ -2,14 +2,17 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { BudgetService } from '@data-access/budget.service';
 import { GroupComponent } from './components/group/group';
 import { SummaryComponent } from './components/summary/summary';
+import { PrintError } from 'src/app/components/app-print-error/app-print-error';
 
 @Component({
   selector: 'app-report',
-  imports: [ GroupComponent, SummaryComponent ],
+  imports: [ GroupComponent, SummaryComponent, PrintError ],
   templateUrl: './report.html',
   styleUrl: './report.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Report {
+  #budgetService = inject(BudgetService);
+  error = this.#budgetService.entryResource.error;
   groups = inject(BudgetService).groups;
 }
